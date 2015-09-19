8852c3447fabf7aebec9c56f495090cbfa351504
Jo Bates <cincinesh@icloud.com>
2015-09-19 10:16:38 -0500

    Updated tue/master/docs

--- tue/master/docs/search/classes_0.js
+++ tue/master/docs/search/classes_0.js
@@ -1,5 +1,8 @@
 var searchData=
 [
+  ['is_5farithmetic_5fsimd_5fcomponent',['is_arithmetic_simd_component',['../group__simd__hpp.html#structtue_1_1is__arithmetic__simd__component',1,'tue']]],
+  ['is_5ffloating_5fpoint_5fsimd_5fcomponent',['is_floating_point_simd_component',['../group__simd__hpp.html#structtue_1_1is__floating__point__simd__component',1,'tue']]],
+  ['is_5fintegral_5fsimd_5fcomponent',['is_integral_simd_component',['../group__simd__hpp.html#structtue_1_1is__integral__simd__component',1,'tue']]],
   ['is_5fsimd_5fcomponent',['is_simd_component',['../group__simd__hpp.html#structtue_1_1is__simd__component',1,'tue']]],
   ['is_5fsized_5fbool',['is_sized_bool',['../group__sized__bool__hpp.html#structtue_1_1is__sized__bool',1,'tue']]]
 ];
