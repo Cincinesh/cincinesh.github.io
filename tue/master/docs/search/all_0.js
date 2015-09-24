10019daa8760dbaa1c3ba779de09e73ac74a7508
Jo Bates <cincinesh@icloud.com>
2015-09-23 19:36:12 -0500

    Updated tue/master/docs

--- tue/master/docs/search/all_0.js
+++ tue/master/docs/search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['a',['a',['../group__vec__hpp.html#a9460142d95715a93711ee0886e687a39',1,'tue::vec']]],
-  ['abs',['abs',['../group__mat__hpp.html#gadcbdd155662b3a70db96c6271d3e65b0',1,'tue::math::abs(const mat&lt; T, C, R &gt; &amp;m) noexcept'],['../group__math__hpp.html#ga57105a0015c0d47303e63082531de129',1,'tue::math::abs(T x) noexcept'],['../group__simd__hpp.html#gad79cc34e4b23dd45d5ffbfe02f56546b',1,'tue::math::abs(const simd&lt; T, N &gt; &amp;s) noexcept'],['../group__vec__hpp.html#ga97f8c794a7f462e86b4f5b71ed957a70',1,'tue::math::abs(const vec&lt; T, N &gt; &amp;v) noexcept']]],
+  ['abs',['abs',['../group__mat__hpp.html#gadf0fa9acb7cbd0cf42c79a515753942a',1,'tue::math::abs(const mat&lt; T, C, R &gt; &amp;m) noexcept'],['../group__math__hpp.html#ga57105a0015c0d47303e63082531de129',1,'tue::math::abs(T x) noexcept'],['../group__simd__hpp.html#gad79cc34e4b23dd45d5ffbfe02f56546b',1,'tue::math::abs(const simd&lt; T, N &gt; &amp;s) noexcept'],['../group__vec__hpp.html#ga1aaff5a01fc7b83edbc02fd02a4410c2',1,'tue::math::abs(const vec&lt; T, N &gt; &amp;v) noexcept']]],
   ['axis_5fangle',['axis_angle',['../group__transform__hpp.html#ga2f034182e2bf260bcaf8160e99ce950f',1,'tue::transform::axis_angle(const T &amp;x, const T &amp;y, const T &amp;z) noexcept'],['../group__transform__hpp.html#gaada11e7ef981811658d37f39a757b8d6',1,'tue::transform::axis_angle(const vec3&lt; T &gt; &amp;v) noexcept']]]
 ];
--- tue/master/docs/search/functions_2.js
+++ tue/master/docs/search/functions_2.js
@@ -4,6 +4,6 @@ var searchData=
   ['columns',['columns',['../group__mat__hpp.html#a283acca55758c76109596c9869951243',1,'tue::mat::columns() const  noexcept'],['../group__mat__hpp.html#a74e803f188c46a4fec0307345472e323',1,'tue::mat::columns() noexcept']]],
   ['comp_5fmult',['comp_mult',['../group__mat__hpp.html#gaa37f59e665e253f02c579b554cf4ffa2',1,'tue::math']]],
   ['conjugate',['conjugate',['../group__quat__hpp.html#gac5327e8e61c17ed247b53373dbdb2824',1,'tue::math']]],
-  ['cos',['cos',['../group__mat__hpp.html#ga01de421d5c8b70b8c34dde09d2f2aa0e',1,'tue::math::cos(const mat&lt; T, C, R &gt; &amp;m) noexcept'],['../group__math__hpp.html#gae9e7591ef262a9ca661c48dc0bfc09d1',1,'tue::math::cos(T x) noexcept'],['../group__simd__hpp.html#ga6688cd30a00a847c5e75b5f1241f92ea',1,'tue::math::cos(const simd&lt; T, N &gt; &amp;s) noexcept'],['../group__vec__hpp.html#ga50662579b8b31058a6ca4e5e636a5e13',1,'tue::math::cos(const vec&lt; T, N &gt; &amp;v) noexcept']]],
+  ['cos',['cos',['../group__mat__hpp.html#ga58ca317305f155313cbab62f1cab8ddd',1,'tue::math::cos(const mat&lt; T, C, R &gt; &amp;m) noexcept'],['../group__math__hpp.html#gae9e7591ef262a9ca661c48dc0bfc09d1',1,'tue::math::cos(T x) noexcept'],['../group__simd__hpp.html#ga6688cd30a00a847c5e75b5f1241f92ea',1,'tue::math::cos(const simd&lt; T, N &gt; &amp;s) noexcept'],['../group__vec__hpp.html#gaea9bbc314613975b7690924832b45d59',1,'tue::math::cos(const vec&lt; T, N &gt; &amp;v) noexcept']]],
   ['cross',['cross',['../group__vec__hpp.html#gae4ce96bf8cd32abe61070507b768c99c',1,'tue::math']]]
 ];
