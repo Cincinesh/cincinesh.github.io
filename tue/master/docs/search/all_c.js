dc6a57d356c8badb4c9ad69636ae6f1c85c0d6bb
Jo Bates <cincinesh@icloud.com>
2015-09-27 17:35:24 -0500

    Updated tue/master/docs

--- tue/master/docs/search/all_c.js
+++ tue/master/docs/search/all_c.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['perspective_5fmat',['perspective_mat',['../group__transform__hpp.html#gaa9278e7e108327357cfb32c0a7e7c69c',1,'tue::transform']]],
+  ['perspective_5fmat',['perspective_mat',['../group__transform__hpp.html#ga52cc44c3b4a5c129dae0a03594b7cbd5',1,'tue::transform']]],
   ['pow',['pow',['../group__mat__hpp.html#ga254225b7863108d2cef2730afddd1baa',1,'tue::math::pow(const mat&lt; T, C, R &gt; &amp;bases, const mat&lt; T, C, R &gt; &amp;exponents) noexcept'],['../group__math__hpp.html#ga6fd1025708347a2f526ae3db374c663a',1,'tue::math::pow(T x, T y) noexcept'],['../group__simd__hpp.html#ga2e5311fd1868a3641e0af2cbab64ed27',1,'tue::math::pow(const simd&lt; T, N &gt; &amp;bases, const simd&lt; T, N &gt; &amp;exponents) noexcept'],['../group__vec__hpp.html#ga1a41da73c440e6cf035cdb928773a386',1,'tue::math::pow(const vec&lt; T, N &gt; &amp;bases, const vec&lt; T, N &gt; &amp;exponents) noexcept']]]
 ];
