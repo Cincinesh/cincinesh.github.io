8378362f8daca3a1ff7f72f7d9afba2c1c874abe
Jo Bates <cincinesh@icloud.com>
2015-09-27 11:12:19 -0500

    Updated tue/master/docs

--- tue/master/docs/search/functions_a.js
+++ tue/master/docs/search/functions_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['nocopy_5fcast',['nocopy_cast',['../group__nocopy__cast__hpp.html#ga527a1151661097e55f119a04d68071c6',1,'tue']]],
-  ['normalize',['normalize',['../group__quat__hpp.html#ga10327bcc4030cfe7f4e551b0c95a6693',1,'tue::math::normalize(const quat&lt; T &gt; &amp;q) noexcept'],['../group__vec__hpp.html#gafa681fdbc79e7f9d1a7bc746e378189c',1,'tue::math::normalize(const vec&lt; T, N &gt; &amp;v) noexcept']]],
+  ['normalize',['normalize',['../group__quat__hpp.html#gae79edd555eb6a1872071fa70de3fe900',1,'tue::math::normalize(const quat&lt; T &gt; &amp;q) noexcept'],['../group__vec__hpp.html#gafa681fdbc79e7f9d1a7bc746e378189c',1,'tue::math::normalize(const vec&lt; T, N &gt; &amp;v) noexcept']]],
   ['not_5fequal',['not_equal',['../group__mat__hpp.html#ga22e9bbb204c5746bff6e491b507c25d4',1,'tue::math::not_equal(const mat&lt; T, C, R &gt; &amp;lhs, const mat&lt; T, C, R &gt; &amp;rhs) noexcept'],['../group__math__hpp.html#ga84c3afb7395b7c4d925b98a5d18c2a6b',1,'tue::math::not_equal(T lhs, T rhs) noexcept'],['../group__simd__hpp.html#ga5514ee5f814e24ab181b2c87d82ab8e3',1,'tue::math::not_equal(const simd&lt; T, N &gt; &amp;lhs, const simd&lt; T, N &gt; &amp;rhs) noexcept'],['../group__vec__hpp.html#ga9f0ddc0f97fccf1e05e886acf9c62adb',1,'tue::math::not_equal(const vec&lt; T, N &gt; &amp;lhs, const vec&lt; T, N &gt; &amp;rhs) noexcept']]]
 ];
